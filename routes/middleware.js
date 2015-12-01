fffedab950768114250ab1adc54c61db301a4b66
Logan Bernard <bernard.logan4@gmail.com>
2015-11-30 19:51:12 -0500

    [landing] about and workshops

--- routes/middleware.js
+++ routes/middleware.js
@@ -24,7 +24,7 @@ exports.initLocals = function(req, res, next) {
 	var locals = res.locals;
 	
 	locals.navLinks = [
-		{ label: 'Home',		key: 'home',		href: '/' },
+		// { label: 'Home',		key: 'home',		href: '/' },
 		{ label: 'Blog',		key: 'blog',		href: '/blog' },
 		{ label: 'Gallery',		key: 'gallery',		href: '/gallery' },
 		{ label: 'Contact',		key: 'contact',		href: '/contact' }
--- routes/views/index.js
+++ routes/views/index.js
@@ -8,6 +8,32 @@ exports = module.exports = function(req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
+	// locals.workshops = [
+	// 	{
+	// 		title: 'Refine & Align',
+	// 		studio: 'Leap Yoga',
+	// 		location: 'Folsom, CA',
+	// 		date: '11/20/15'
+	// 	},
+	// 	{
+	// 		title: 'Refine & Align',
+	// 		studio: 'Leap Yoga',
+	// 		location: 'Folsom, CA',
+	// 		date: '11/20/15'
+	// 	}
+	// ];
+
+	view.on('init', function(next) {
+
+		keystone.list('Post')
+			.model
+			.findOne({ title: 'Workshops' })
+			.exec(function(err, result) {
+				locals.workshops = result;
+				next(err);
+			});
+
+	});
 	
 	// Render the view
 	view.render('index');
